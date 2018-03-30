e0be0b15ebd50ebc42044e1674cf6f1fa7748b3a
chan <1355425625@qq.com>
2018-03-30 13:44:48 +0800

    Add files via upload

---
+++ make/encode/c.h
@@ -0,0 +1,20 @@
+//
+// Created by chan on 2018/3/30.
+//
+
+#ifndef YASCFAIRY_C_H
+#define YASCFAIRY_C_H
+
+static const char *pkg_md5_map[][2] = {
+        {"com.shanbay.words",         "0712f2376039dab3966e6714e6c55d94"},
+        {"com.shanbay.listen",        "0712f2376039dab3966e6714e6c55d94"},
+        {"com.shanbay.sentence",      "0712f2376039dab3966e6714e6c55d94"},
+        {"com.shanbay.news",          "0712f2376039dab3966e6714e6c55d94"},
+        {"com.shanbay.reader",        "0712f2376039dab3966e6714e6c55d94"},
+        {"com.shanbay.speak",         "0712f2376039dab3966e6714e6c55d94"},
+        {"com.shanbay.fairies",       "0712f2376039dab3966e6714e6c55d94"},
+        {"com.shanbay.security.demo", "0712f2376039dab3966e6714e6c55d94"},
+        {"com.shanbay.fairy.yasc",    "4fec59d7a12cac1bed468282cea98478"},
+};
+
+#endif //YASCFAIRY_C_H
