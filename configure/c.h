528991c63af6011a32f938a72a6fa2ff51a3fda9
ChanJLee <1355425625@qq.com>
2018-04-08 22:10:19 +0800

    sync

--- configure/c.h
+++ configure/c.h
@@ -6,13 +6,6 @@
 #define YASCFAIRY_C_H
 
 static const char *pkg_md5_map[][2] = {
-        {"com.shanbay.words",         "0712f2376039dab3966e6714e6c55d94"},
-        {"com.shanbay.listen",        "0712f2376039dab3966e6714e6c55d94"},
-        {"com.shanbay.sentence",      "0712f2376039dab3966e6714e6c55d94"},
-        {"com.shanbay.news",          "7a85fc48b67d5a70fb2daa780b0c5f93"},
-        {"com.shanbay.reader",        "0712f2376039dab3966e6714e6c55d94"},
-        {"com.shanbay.speak",         "0712f2376039dab3966e6714e6c55d94"},
-        {"com.shanbay.fairies",       "0712f2376039dab3966e6714e6c55d94"},
 };
 
 #endif //YASCFAIRY_C_H
